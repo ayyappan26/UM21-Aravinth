1c914221cc850ce0ef436044c4156feba64f388b
Black <Black@DESKTOP-1LMPSE3>
2021-09-01 12:39:49 +0530

    Saving Constructor

--- UltramainIntenshipJavaProjects/src/com/ultramain/um21/constructor/TestConstructor.java
+++ UltramainIntenshipJavaProjects/src/com/ultramain/um21/constructor/TestConstructor.java
@@ -6,7 +6,7 @@ public class TestConstructor {
 		System.out.println("hii....");
 	}
 	public static void main(String [] args){
-	System.out.println("hiii...");
+	System.out.println("hiii... Hello....");
 	}
 
 }
